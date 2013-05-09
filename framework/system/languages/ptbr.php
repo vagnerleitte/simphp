0f69c5e8247c59c396e9e8aef23e067231856472
Vagner Leitte <vagner.leite.silva@gmail.com>
2013-05-09 20:08:07 -0300

    Definicoes e melhorias no codigo original do framework

---
+++ framework/system/languages/ptbr.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ *
+ *
+ * PHP 5
+ *
+ * Sistema : Miniframework PHP OO para aplicações web.
+ *
+ *
+ *
+ * @copyright     Copyright 2013-2014, Vagner Leite http://vagnerleitte.github.io/simphp/
+ * @author		  Vagner Leite (vagnerleitte@outlook.com)
+ * @created 	  03-12-2012
+ * @link          http://vagnerleitte.github.io/simphp/
+ * @version       Sistema v 0.1
+ * @package		  root/system
+ */
+
+ 
+  
+/**
+ * String de idioma do seu site. É possível criar um site multi-idiomas utilizando arquivos de tradução
+ *
+ */
